270647b131447c8f57e7cac3b5e55a09714118fc
Dias <dias.atudinov@gmail.com>
2023-04-20 00:27:00 +0600

    Initial Commit

---
+++ GBShop/GBShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBShopApp.swift
+//  GBShop
+//
+//  Created by Atudinov Dias on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GBShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
